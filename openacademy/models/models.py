57250e4df657e87c471518493bad88c0ce56a8d4
rafikis23 <leocasco23@gmail.com>
2020-07-24 16:00:37 -0600

    fix

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -28,8 +28,10 @@ class Session(models.Model):
     seats = fields.Integer(string="Number of seats")
 
     instructor_id = fields.Many2one('res.partner', string="Instructor",
-        domain=['|', ('instructor','=',True),
-                     ('category_id.name','ilike',"Teacher")])
+        domain=['|', ('instructor', '=', True),
+                     ('category_id.name', 'ilike', "Teacher")])
+
+
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
