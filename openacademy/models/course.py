3827d80479d84f6fa9794f7bbf64042316884cde
rafikis23 <leocasco23@gmail.com>
2020-07-24 14:43:31 -0600

    [ADD] nine commit
    Relational fields

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -10,6 +10,13 @@ class Course(models.Model):
     description = fields.Text()
 
 
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many(
+        'openacademy.session', 'course_id', string="Sessions")
+
+
+
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -19,3 +26,9 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course',
+        ondelete='cascade', string="Course", required=True)
+    attemdee_ids = fields.Many2many('res.partner', string="Attendees")
+ 
